63846cf90de1fc709280e9b4f6a52973dac860db
Joel Pimentel <joelpimentel1995@gmail.com>
2021-08-22 18:32:27 -0500

    modo dark

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 const $form = document.querySelector(".form");
-const $tb = document.getElementById("tbody");
 const $table = document.querySelector(".table");
+const $main = document.querySelector('.main')
 const $actionFilter = document.getElementById('action__filter')
+const btn_theme = document.querySelector('.btn_theme')
+
+const darkTheme = {
+"--img--header": "url(./images/bg-mobile-dark.jpg)",
+"--icon--theme": "url(./images/icon-sun.svg)",
+"--bg--main":"hsl(235, 24%, 19%",
+"--input-bg":" hsl(235, 24%, 19%)",
+"--text-table-color":"hsl(234, 39%, 85%)",
+"--text-filter-color":"hsl(234, 11%, 52%)",
+"--bg-body":" hsl(235, 21%, 11%)",
+"--color-border-botton":"hsl(237, 14%, 26%)",
+"--color-deleted":"hsl(233, 14%, 35%)",
+}
+
+const lightTheme = {
+"--img--header": "url(./images/bg-mobile-light.jpg)",
+"--icon--theme": "url(./images/icon-moon.svg)",
+"--bg--main":"hsl(0, 0%, 98%)",
+"--input-bg":"hsl(0, 0%, 98%)",
+"--text-table-color":"hsl(235, 1%, 35%)",
+"--text-filter-color":"hsl(236, 9%, 61%)",
+"--bg-body":" hsl(236, 33%, 92%)",
+"--color-border-botton":"hsl(233, 11%, 84%)",
+"--color-deleted":"hsl(233, 11%, 84%)",
+}
+  
+
 
 window.addEventListener("DOMContentLoaded", () => {
   renderTable(tasks);
@@ -36,32 +63,36 @@ let $frgmt = document.createDocumentFragment();
 
 const renderTable = (n) => {
   console.log(n)
-  $tb.innerHTML = "";
+  $table.innerHTML = "";
   n.forEach((item) => {
-    $tb.innerHTML += `
-     <tr >
-        <th data-id=${item.id}>
-        <input type="checkbox"  name="checked" ${
-        item.done ? "checked" : "" }>
-        <td >${item.task}</td>
-        </th>
-      </tr>
+    $table.innerHTML += `
+ <div  class="rows">
+  <div data-id=${item.id}>
+    <input type="checkbox" class="input__check"  name="checked" ${
+      item.done ? "checked" : "" }>
+    <p class="title__task">${item.task}</p>
+  </div>
+     <span class="deleted">x</span>
+  </div>
+
         `;
   });
-  $table.appendChild($tb);
+ // $main.insertAdjacentElement('afterbegin', $table);
   
 };
 ///event the table body 
-$tb.addEventListener("click", (e) => {
+$table.addEventListener("click", (e) => {
+ 
   if (e.target.name === "checked") {
     //console.log(e.target.checked)
     taskId = e.target.parentNode.dataset.id;
+    console.log(taskId)
     const newArray = tasks.map((task) =>
       task.id == taskId ? { ...task, done: !task.done } : task
     );
     tasks = newArray;   
   }
- 
+  console.log(tasks)
 });
 
 //event filter 
@@ -89,4 +120,17 @@ const filterTasks=(n, typeFilter, clear)=>{
     }
 }
 
+let switchTheme = false
+btn_theme.addEventListener('click', (e)=>{
+    switchTheme = !switchTheme;
+    switchTheme? changeTheme(lightTheme): changeTheme(darkTheme)
+})
+
+const changeTheme=(theme)=>{
+  for (const key in theme) {
+        console.log(theme[key])
+        document.documentElement.style.setProperty(key, theme[key])
+    }
+  
+}
 
